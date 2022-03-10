7bbf8ab817ab5a718ba011a52d0c6d7459cd7c5c
Rozey17 <ferrad.fm@gmail.com>
2022-03-10 14:20:15 +0100

    chore: wip

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '11f07f5195e4a92383860828e15b50db'),
+    },
+  },
+});
